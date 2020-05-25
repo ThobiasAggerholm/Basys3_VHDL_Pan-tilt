480cd3396aa86982ec03b2a344ab5388e3e83afd
ThobiasMSA <thagg18@student.sdu.dk>
2020-05-25 15:14:16 +0200

    switch input for pin test

--- Projekt_4_Basys3.srcs/sources_1/imports/new/STANDARD_TOPLEVEL.vhd
+++ Projekt_4_Basys3.srcs/sources_1/imports/new/STANDARD_TOPLEVEL.vhd
@@ -66,21 +66,9 @@ entity STANDARD_TOPLEVEL is
            IN1B     : out STD_LOGIC;
            IN2B     : out STD_LOGIC;
            
-           --Simulations porte:
-           -- Positions countere
-           POS1    : out std_logic_vector (Hall_Counter_size-1 downto 0);
-           POS2    : out std_logic_vector (Hall_Counter_size-1 downto 0);
            
-           --SPI Buffers
-           RECEIVE_BUFFER : out std_logic_vector(SPI_data_length-1 downto 0);
-           TRANSMIT_BUFFER : out std_logic_vector(SPI_data_length-1 downto 0);
-           
-           --PWM buffer and puls
-           PWM_DUTYCYCLE1 : out std_logic_vector( PWM_resolution-1 downto 0);
-           PWM_DUTYCYCLE2 : out std_logic_vector( PWM_resolution-1 downto 0);
-           
-           PWM_PULSE1 : out std_logic;
-           PWM_PULSE2 : out std_logic    
+           --PWM test
+           sw : in std_logic_vector(SPI_data_length -1 downto 0)    
            );
 end STANDARD_TOPLEVEL;
 
@@ -114,16 +102,8 @@ architecture Behavioral of STANDARD_TOPLEVEL is
     clk : in STD_LOGIC;
     Chan_A : in STD_LOGIC := '0';
     Chan_B : in STD_LOGIC := '0';
-    Hall_Counter : out STD_LOGIC_VECTOR (Hall_Counter_size-1 downto 0);
+    Hall_Counter : out STD_LOGIC_VECTOR (Hall_Counter_size-1 downto 0)
     
-    Tdirection : out std_logic;
-    TA_prev : out std_logic;
-    TB_prev : out std_logic;
-    TA_curr : out std_logic;
-    TB_curr : out std_logic;
-    
-    TA_in : out std_logic_vector(1 downto 0);
-    TB_in : out std_logic_vector(1 downto 0)
     );
     end component;
     
@@ -178,16 +158,7 @@ begin
     port map (  clk => clk,
                 Chan_A => CHAN_A1,
                 Chan_B => CHAN_B1,
-                Hall_counter => motor1_out, 
-                
-                Tdirection => open,
-                TA_prev => open,
-                TB_prev => open,
-                TA_curr => open,
-                TB_curr => open,
-                
-                TA_in => open,
-                TB_in => open
+                Hall_counter => motor1_out
     );
     
     
@@ -196,16 +167,7 @@ begin
     port map (  clk => clk,
                 Chan_A => CHAN_A2,
                 Chan_B => CHAN_B2,
-                Hall_counter => motor2_out, 
-                
-                Tdirection => open,
-                TA_prev => open,
-                TB_prev => open,
-                TA_curr => open,
-                TB_curr => open,
-                
-                TA_in => open,
-                TB_in => open
+                Hall_counter => motor2_out
     );
     
     Motor1 : H_bridge
@@ -226,42 +188,31 @@ begin
                PWM_pulse => PWM_pulseB
     );
     
- -- SIMULATION PORTS:
- POS1 <= motor1_out;
- POS2 <= motor2_out;
- 
- TRANSMIT_BUFFER<= Data_Tra_Queue;
- RECEIVE_BUFFER <= Data_Rec_Queue;
- 
- PWM_DUTYCYCLE1 <= PWM_dutyCycleA;
- PWM_DUTYCYCLE2 <= PWM_dutyCycleB;
- 
- PWM_PULSE1 <= PWM_pulseA;
- PWM_PULSE2 <= PWM_pulseB;
+
   
   --Transmit
-  Data_Tra_Queue(Hall_Counter_size + 2 downto Hall_Counter_size) <= Hall1 & Hall0 & Data_Rec_Queue(10);
+  Data_Tra_Queue(2 downto 0) <= sw(2) & Hall0 & Hall1;
   
   transmit : process(clk)
   begin
   --When data is ready to read, transmission is done.
-  if Data_Rec_Queue(10) = '1' then
-  Data_Tra_Queue(Hall_Counter_size -1 downto 0) <= motor2_out;
+  if sw(2) = '1' then
+  Data_Tra_Queue(Hall_Counter_size + 3 downto 3) <= motor2_out & dirB;
   else
-  Data_Tra_Queue(Hall_Counter_size -1 downto 0) <= motor1_out;
+  Data_Tra_Queue(Hall_Counter_size + 3 downto 3) <= motor1_out & dirA;
   end if;
   end process;
   
   recieve : process(clk)
   begin
-    if Data_Rec_Queue(10) = '1' then --MotorSelect - Enable - direction - PWM 8 bit
-        enableA <= Data_Rec_Queue(9);
-        dirA <= Data_Rec_Queue(8);
-        PWM_dutyCycleA <= Data_Rec_Queue(PWM_resolution -1 downto 0);          
+    if sw(2) = '1' then --MotorSelect - Enable - direction - PWM 8 bit
+        enableA <= sw(0);
+        dirA <= sw(1);
+        PWM_dutyCycleA <= sw(PWM_resolution + 2 downto 3);          
     else
-        enableB <= Data_Rec_Queue(9);
-        dirB <= Data_Rec_Queue(8);
-        PWM_dutyCycleB <= Data_Rec_Queue(PWM_resolution -1 downto 0);    
+        enableB <= sw(0);
+        dirB <= sw(1);
+        PWM_dutyCycleB <= Data_Rec_Queue(PWM_resolution + 2 downto 3);    
         
     end if;
   end process;
